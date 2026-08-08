41af0809d2f180ecebe403597a863bcfb8a257f0
Tan Zheng Lin <54979360+graystick@users.noreply.github.com>
2026-08-08 09:28:23 +0800

    Update MainProgram.py
    Added Binary Converter Function

--- MainProgram.py
+++ MainProgram.py
@@ -5,3 +5,17 @@ Created on Sat Aug  8 09:23:28 2026
 @author: rtan5
 """
 
+import cv2
+import numpy as np
+from matplotlib import pyplot as pt
+
+def pageToBinary(image_path): ##take a page from the folder and converts it to grayscale, then thresholds it to become a binary image for processing the text
+    page = cv2.imread(image_path)
+    pageGrayscale = cv2.cvtColor(page, cv2.COLOR_BGR2GRAY)
+    
+    _, pageBinary = cv2.threshold(pageGrayscale, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+    return page, pageBinary
+
+
+
+
